feb0c45052c246cb3b2979fd9d0ae526549b5312
randomtoybox01 <colecheif@gmail.com>
2019-05-09 12:57:06 -0600

    Added communication between clients

--- ChatServer/src/com/muc/ServerWorker.java
+++ ChatServer/src/com/muc/ServerWorker.java
@@ -3,6 +3,7 @@ package com.muc;
 import org.apache.commons.lang3.StringUtils;
 import java.io.*;
 import java.net.Socket;
+import java.util.HashSet;
 import java.util.List;
 
 public class ServerWorker extends Thread {
@@ -11,6 +12,7 @@ public class ServerWorker extends Thread {
     private final Server server;
     private String login = null;
     private OutputStream outputStream;
+    private HashSet<String> topicSet = new HashSet<>();
 
     public ServerWorker(Server server, Socket clientSocket) {
         this.server = server;
@@ -38,10 +40,16 @@ public class ServerWorker extends Thread {
             String[] tokens = StringUtils.split(line);
             if (tokens != null && tokens.length > 0) {
                 String cmd = tokens[0];
-                if ("quit".equalsIgnoreCase(line)) {
+                if ("logoff".equals(cmd) || "quit".equalsIgnoreCase(line)) {
+                    handleLogoff();
                     break;
-                }else if("login".equalsIgnoreCase(cmd)){
+                } else if ("login".equalsIgnoreCase(cmd)) {
                     handleLogin(outputStream, tokens);
+                } else if ("msg".equalsIgnoreCase(cmd)) {
+                    String[] tokensMsg = StringUtils.split(line, null, 3);
+                    handleMessge(tokensMsg);
+                } else if("join".equalsIgnoreCase(cmd)) {
+                    handleJoin(tokens);
                 } else {
                     String msg = "unknown " + cmd + "\n";
                     outputStream.write(msg.getBytes());
@@ -52,6 +60,59 @@ public class ServerWorker extends Thread {
         }
 
         clientSocket.close();
+    }
+
+    private void handleLeave(String[] tokens) {
+        if (tokens.length > 1) {
+            String topic = tokens[1];
+            topicSet.remove(topic);
+        }
+    }
+
+    public boolean isMemberOfTopic(String topic) {
+        return topicSet.contains(topic);
+    }
+
+    private void handleJoin(String[] tokens) {
+        if (tokens.length > 1) {
+            String topic = tokens[1];
+            topicSet.add(topic);
+
+        }
+    }
+
+    private void handleMessge(String[] tokens) throws IOException {
+        String sendTo = tokens[1];
+        String body = tokens[2];
+
+        boolean isTopic = sendTo.charAt(0) == '#';
+
+        List<ServerWorker> workerList = server.getWorkerList();
+        for (ServerWorker worker : workerList) {
+            if(isTopic) {
+                if (worker.isMemberOfTopic(sendTo)) {
+                    String outMsg =  "msg" + login + " " + body + "\n";
+                    worker.send(outMsg);
+                }
+        } else {
+                if (sendTo.equalsIgnoreCase(worker.getLogin())) {
+                String outMsg =  "msg" + login + " " + body + "\n";
+                worker.send(outMsg);
+            }
+            }
+        }
+    }
+
+    private void handleLogoff() throws IOException {
+        server.removeWorker(this);
+        List<ServerWorker> workerlist = server.getWorkerList();
+
+        String onlineMsg = "logout" + login + "\n";
+        for (ServerWorker worker : workerlist) {
+            if (!login.equals(worker.getLogin())) {
+                worker.send(onlineMsg);
+            }
+        }
     }
 
     public String getLogin() {
@@ -69,8 +130,16 @@ public class ServerWorker extends Thread {
                 this.login = login;
                 System.out.println("User logged in successfully: " + login);
 
-                String onlineMsg = "online " + login + "\n";
                 List<ServerWorker> workerList = server.getWorkerList();
+                for(ServerWorker worker : workerList) {
+                    if (worker.getLogin() != null) {
+                        if (!login.equals(worker.getLogin())) {
+                            String msg2 = "online" + worker.getLogin();
+                            send(msg2);
+                        }
+                    }
+                }
+                String onlineMsg = "online" + login + "\n";
                 for(ServerWorker worker : workerList) {
                     worker.send(onlineMsg);
                 }
@@ -83,6 +152,8 @@ public class ServerWorker extends Thread {
     }
 
     private void send(String onlineMsg) throws IOException {
-        outputStream.write(onlineMsg.getBytes());
+        if (login != null) {
+            outputStream.write(onlineMsg.getBytes());
+        }
     }
 }
